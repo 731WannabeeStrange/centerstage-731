744a76f78f254e20b54fd366bcb403fc7feca219
Ari Singh <arisingh8@gmail.com>
2024-01-27 16:45:04 -0500

    finishing vision odometry, lie extensions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LieExtensions.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LieExtensions.kt
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.utils
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.Twist2d
 import com.acmerobotics.roadrunner.Vector2d
-import org.ejml.data.DMatrix2x2
 import org.ejml.data.DMatrix3x3
 import org.ejml.data.DMatrixRMaj
 import org.ejml.dense.fixed.CommonOps_DDF3
@@ -11,90 +10,91 @@ import kotlin.math.cos
 import kotlin.math.pow
 import kotlin.math.sin
 
-fun Twist2d.rightJacobian(): DMatrix3x3 {
-    return if (angle == 0.0) {
-        DMatrix3x3(
+fun Twist2d.rightJacobian(matrix: DMatrix3x3) {
+    if (angle == 0.0) {
+        matrix.set(
             0.0, 0.0, 0.0,
             0.0, 0.0, 0.0,
             0.0, 0.0, 1.0
         )
     } else {
-        DMatrix3x3(
-            sin(angle) / angle, (1 - cos(angle)) / angle, (angle * line.x - line.y + line.y * cos(angle) - line.x * sin(angle)) / angle.pow(2),
-            (cos(angle) - 1) / angle, sin(angle) / angle, (line.x + angle * line.y - line.x * cos(angle) - line.y * sin(angle)) / angle.pow(2),
-            0.0, 0.0, 1.0
+        matrix.set(
+            sin(angle) / angle,
+            (1 - cos(angle)) / angle,
+            (angle * line.x - line.y + line.y * cos(angle) - line.x * sin(angle)) / angle.pow(2),
+            (cos(angle) - 1) / angle,
+            sin(angle) / angle,
+            (line.x + angle * line.y - line.x * cos(angle) - line.y * sin(angle)) / angle.pow(2),
+            0.0,
+            0.0,
+            1.0
         )
     }
 }
 
-fun Twist2d.leftJacobian(): DMatrix3x3 {
-    return if (angle == 0.0) {
-        DMatrix3x3(
+fun Twist2d.leftJacobian(matrix: DMatrix3x3) {
+    if (angle == 0.0) {
+        matrix.set(
             0.0, 0.0, 0.0,
             0.0, 0.0, 0.0,
             0.0, 0.0, 1.0
         )
     } else {
-        DMatrix3x3(
-            sin(angle) / angle, (cos(angle) - 1) / angle, (angle * line.x + line.y - line.y * cos(angle) - line.x * sin(angle)) / angle.pow(2),
-            (1 - cos(angle)) / angle, sin(angle) / angle, (-line.x + angle * line.y + line.x * cos(angle) - line.y * sin(angle)) / angle.pow(2),
-            0.0, 0.0, 1.0
+        matrix.set(
+            sin(angle) / angle,
+            (cos(angle) - 1) / angle,
+            (angle * line.x + line.y - line.y * cos(angle) - line.x * sin(angle)) / angle.pow(2),
+            (1 - cos(angle)) / angle,
+            sin(angle) / angle,
+            (-line.x + angle * line.y + line.x * cos(angle) - line.y * sin(angle)) / angle.pow(2),
+            0.0,
+            0.0,
+            1.0
         )
     }
 }
 
 // check this carefully later
-fun Pose2d.adjoint(): DMatrix3x3 {
-    return DMatrix3x3(
+fun Pose2d.adjoint(matrix: DMatrix3x3) {
+    matrix.set(
         heading.real, -heading.imag, position.y,
         heading.imag, heading.real, -position.x,
         0.0, 0.0, 1.0
     )
 }
 
-fun Pose2d.plusJacobians(other: Twist2d): PlusResult {
+fun Pose2d.plusJacobians(other: Twist2d, jSelf: DMatrix3x3, jTau: DMatrix3x3): Pose2d {
     val tau = Pose2d.exp(other)
-    return PlusResult(
-        times(tau),
-        tau.inverse().adjoint(),
-        other.rightJacobian()
-    )
+    tau.inverse().adjoint(jSelf)
+    other.rightJacobian(jTau)
+    return times(tau)
 }
 
-fun Pose2d.inverseJacobians(): InverseResult {
-    val jSelf = adjoint()
+fun Pose2d.inverseJacobians(jSelf: DMatrix3x3): Pose2d {
+    adjoint(jSelf)
     CommonOps_DDF3.changeSign(jSelf)
-    return InverseResult(
-        inverse(),
-        jSelf
-    )
+    return inverse()
 }
 
-fun Pose2d.timesJacobians(other: Vector2d): PointsComposeResult {
-    val jSelf = DMatrixRMaj(
+fun Pose2d.timesJacobians(other: Vector2d, jSelf: DMatrixRMaj): Vector2d {
+    jSelf.set(
         arrayOf(
-            doubleArrayOf(heading.real, -heading.imag, -position.x * heading.imag - position.y * heading.real),
-            doubleArrayOf(heading.imag, heading.real, position.x * heading.real - position.y * heading.imag)
+            doubleArrayOf(
+                heading.real,
+                -heading.imag,
+                -position.x * heading.imag - position.y * heading.real
+            ),
+            doubleArrayOf(
+                heading.imag,
+                heading.real,
+                position.x * heading.real - position.y * heading.imag
+            )
         )
     )
-    return PointsComposeResult(
-        times(other),
-        jSelf,
-        DMatrix2x2(
-            heading.real, -heading.imag,
-            heading.imag, heading.real
-        )
-    )
+    return times(other)
 }
 
-fun Pose2d.timesJacobians(other: Pose2d): PoseComposeResult {
-    return PoseComposeResult(
-        times(other),
-        other.inverse().adjoint()
-    )
+fun Pose2d.timesJacobians(other: Pose2d, jSelf: DMatrix3x3): Pose2d {
+    other.inverse().adjoint(jSelf)
+    return times(other)
 }
-
-data class InverseResult(val pose: Pose2d, val jSelf: DMatrix3x3)
-data class PlusResult(val pose: Pose2d, val jSelf: DMatrix3x3, val jTau: DMatrix3x3)
-data class PointsComposeResult(val point: Vector2d, val jSelf: DMatrixRMaj, val jTau: DMatrix2x2)
-data class PoseComposeResult(val pose: Pose2d, val jSelf: DMatrix3x3)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/localization/VisionOdometryLocalizer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/localization/VisionOdometryLocalizer.kt
@@ -5,12 +5,9 @@ import com.acmerobotics.roadrunner.PoseVelocity2d
 import com.acmerobotics.roadrunner.Twist2d
 import com.acmerobotics.roadrunner.Vector2d
 import com.qualcomm.robotcore.hardware.HardwareMap
-import org.ejml.data.DMatrix2
 import org.ejml.data.DMatrix2x2
-import org.ejml.data.DMatrix3
 import org.ejml.data.DMatrix3x3
 import org.ejml.data.DMatrixRMaj
-import org.ejml.dense.fixed.CommonOps_DDF2
 import org.ejml.dense.row.CommonOps_DDRM
 import org.ejml.equation.Equation
 import org.ejml.equation.Sequence
@@ -44,14 +41,15 @@ class VisionOdometryLocalizer(
     private var J_u = DMatrix3x3()
     private var J_xi_x = DMatrix3x3()
     private var J_e_xi = DMatrixRMaj(2, 3)
-    private val updateCovarianceOdometry: Sequence
-    private val updateH: Sequence
-    private val updateCovarianceCamera: Sequence
-    private val updateKalmanGain: Sequence
-
     private var H = DMatrixRMaj(2, 3)
     private var Z = DMatrix2x2()
     private var K = DMatrixRMaj(3, 2)
+
+    private val updateCovarianceOdometry: Sequence
+    private val updateH: Sequence
+    private val updateCameraCovariance: Sequence
+    private val updateKalmanGain: Sequence
+    private val updateCovarianceMeasurement: Sequence
 
     init {
         val eq = Equation()
@@ -69,48 +67,55 @@ class VisionOdometryLocalizer(
         )
 
         updateCovarianceOdometry = eq.compile("P = F*P*F' + G*W*G'")
-        updateH = eq.compile("H = I*J")
-        updateCovarianceCamera = eq.compile("Z = H*P*H' + R")
+        updateH = eq.compile("H = J*I")
+        updateCameraCovariance = eq.compile("Z = H*P*H' + R")
         updateKalmanGain = eq.compile("K = P*H'*inv(Z)")
+        updateCovarianceMeasurement = eq.compile("P = P - K*Z*K'")
     }
 
     override fun update() {
+        // get odometry signal
         val twist = odometryLocalizer.update()
         poseVelocity = twist.velocity().value()
 
-        val plusResult = poseEstimate.plusJacobians(twist.value())
-        poseEstimate = plusResult.pose
-        J_x = plusResult.jSelf
-        J_u = plusResult.jTau
+        // move
+        poseEstimate = poseEstimate.plusJacobians(twist.value(), J_x, J_u)
         updateCovarianceOdometry.perform() // these steps alone should do odo localization right
 
+        // correct using each landmark
         for (result in aprilTagCameras.getCameraResults()) {
             for (detection in result.aprilTagDetections) {
+                // landmark
                 val b = Vector2d(
                     detection.metadata.fieldPosition.get(0).toDouble(),
                     detection.metadata.fieldPosition.get(1).toDouble()
                 )
 
+                // measurement
                 val y = Vector2d(
                     detection.ftcPose.x,
                     detection.ftcPose.y
                 )
 
-                val invResult = (poseEstimate * result.robotToCameraTransform).inverseJacobians()
-                val actResult = invResult.pose.timesJacobians(b)
-                J_xi_x = invResult.jSelf
-                J_e_xi = actResult.jSelf
-                val e = actResult.point
+                // expectation
+                val e = (poseEstimate * result.robotToCameraTransform).inverseJacobians(J_xi_x)
+                    .timesJacobians(b, J_e_xi)
                 updateH.perform()
 
+                // innovation
                 val z = y - e
-                updateCovarianceCamera.perform()
+                updateCameraCovariance.perform()
 
+                // Kalman gain
                 updateKalmanGain.perform()
 
+                // correction
                 val dx = DMatrixRMaj()
                 CommonOps_DDRM.mult(K, DMatrixRMaj(doubleArrayOf(z.x, z.y)), dx)
+
+                // update
                 poseEstimate += Twist2d(Vector2d(dx.get(0), dx.get(1)), dx.get(2))
+                updateCovarianceMeasurement.perform()
             }
         }
     }
